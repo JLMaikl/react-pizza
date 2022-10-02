8b740e5c97286d3c23cad37be12bca4a38f9c99e
JLMaikl <jlmaikl@mail.ru>
2022-10-02 14:11:20 +0300

    Получает данные с бэкенда + useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
@@ -6,9 +6,19 @@ import Sort from './components/Sort';
 
 import './scss/app.scss';
 
-import pizzas from './assets/pizzas.json';
+// import pizzas from './assets/pizzas.json';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://63395945937ea77bfdc99bb3.mockapi.io/items')
+    .then(res => res.json())
+    .then(arr => setItems(arr))
+  }, []);
+
+
+
   return (
     <>
       <div className="wrapper">
@@ -21,7 +31,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.pizzas.map(obj => {
+            {items.map(obj => {
               return (
                 <PizzaBlock 
                   {...obj}
