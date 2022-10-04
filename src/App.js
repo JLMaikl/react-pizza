8fb16ef38ee175778093e2213a99ec1641367eaf
JLMaikl <jlmaikl@mail.ru>
2022-10-04 10:09:16 +0300

    Реализует Sceleton

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import { useState, useEffect } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
+import Sceleton from './components/PizzaBlock/Sceleton';
 import Sort from './components/Sort';
 
 import './scss/app.scss';
 
-// import pizzas from './assets/pizzas.json';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://63395945937ea77bfdc99bb3.mockapi.io/items')
     .then(res => res.json())
-    .then(arr => setItems(arr))
+    .then(arr => {
+      setItems(arr)
+      setIsLoading(false);
+    })
   }, []);
 
 
@@ -22,28 +26,23 @@ function App() {
   return (
     <>
       <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-          <Categories />
-          <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map(obj => {
-              return (
-                <PizzaBlock 
-                  {...obj}
-                  key={obj.id}
-                />
-              );
-            })}
-
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {isLoading
+                ? [...new Array(6)].map((_, index) => <Sceleton key={index}/>)
+                : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />
+                  )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
---
+++ src/components/PizzaBlock/Sceleton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Sceleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={0}
+    width={280}
+    height={465}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    >
+    <rect x="0" y="220" rx="8" ry="8" width="280" height="30" />
+    <circle cx="130" cy="100" r="100" />
+    <rect x="0" y="265" rx="10" ry="10" width="280" height="88" />
+    <rect x="128" y="369" rx="25" ry="25" width="152" height="45" />
+    <rect x="0" y="375" rx="10" ry="10" width="81" height="35" />
+  </ContentLoader>
+);
+
+export default Sceleton;
