ea6dc40c5d6ea2c284947acd97da198628e46a93
JLMaikl <jlmaikl@mail.ru>
2022-10-05 16:36:34 +0300

    Реализует сортировку на бекенде

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react'
 
-const Categories = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
+const Categories = ({ value, onChangeCategory }) => {
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
-
-  const onClickCategory = (index) => {
-    setActiveIndex(index)
-  }
 
   return (
     <div className="categories">
       <ul>
         {categories.map((category, index) => {
           return (
-            <li 
-              onClick={() => onClickCategory(index)} 
-              className={activeIndex === index ? 'active' : ''} 
-              key={index}
-            >
+            <li
+              onClick={() => onChangeCategory(index)}
+              className={value === index ? 'active' : ''}
+              key={index}>
               {category}
             </li>
           );
@@ -26,6 +18,6 @@ const Categories = () => {
       </ul>
     </div>
   );
-}
+};
 
 export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,22 +9,34 @@ import Sort from '../components/Sort';
 const Home = () => {
       const [items, setItems] = useState([]);
       const [isLoading, setIsLoading] = useState(true);
+      const [categoryId, setCategoryId] = useState(0);
+      const [sortType, setSortType] = useState({
+        name: 'популярности',
+        sortProperty: 'rating'
+      });
 
       useEffect(() => {
-        fetch('https://63395945937ea77bfdc99bb3.mockapi.io/items')
+        setIsLoading(true);
+
+        const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+        const sortBy = sortType.sortProperty.replace('-', '');
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+        fetch(`https://63395945937ea77bfdc99bb3.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
           .then((res) => res.json())
           .then((arr) => {
             setItems(arr);
             setIsLoading(false);
           });
           window.scrollTo(0, 0);
-      }, []);
+      }, [categoryId, sortType]);
+
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(index) => setCategoryId(index)} />
+        <Sort value={sortType} onChangeSort={(index) => setSortType(index)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
