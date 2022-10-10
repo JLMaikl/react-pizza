537b9d6cec2bdde07530905a6c804566f66efea8
JLMaikl <jlmaikl@mail.ru>
2022-10-10 22:10:02 +0300

    Сохраняет параметры фильтрации в URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,32 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useRef } from 'react';
 import axios from 'axios';
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 
 import Categories from '../components/Categories';
 import Pagination from '../components/Pagination';
 import PizzaBlock from '../components/PizzaBlock';
 import Sceleton from '../components/PizzaBlock/Sceleton';
-import Sort from '../components/Sort';
+import Sort, { menuList} from '../components/Sort';
 
 import { SearchContext } from '../App';
 
 
 const Home = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const {categoryId, sort, currentPage} = useSelector(state => state.filter);  
   const sortType = sort.sortProperty;
+  const isSearch = useRef(false);
+  const isMounted = useRef(false);
 
   const { searchValue } = useContext(SearchContext);
 
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // const [currentPage, setCarrentPage] = useState(1);
-
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
@@ -33,23 +36,70 @@ const Home = () => {
     dispatch(setCurrentPage(number))
   }
 
+  const fetchPizzas = () => {
+        setIsLoading(true);
+
+        const order = sortType.includes('-') ? 'asc' : 'desc';
+        const sortBy = sortType.replace('-', '');
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const search = searchValue ? `&search=${searchValue}` : '';
+
+        axios
+          .get(
+            `https://63395945937ea77bfdc99bb3.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+          )
+          .then((res) => {
+            setItems(res.data);
+            setIsLoading(false);
+          });
+
+        window.scrollTo(0, 0);
+  }
+
+  //Если изменили параметры и был первый рендер
+    useEffect(() => {
+      if (isMounted.current) {
+        const queryString = qs.stringify({
+          sortProperty: sort.sortProperty,
+          categoryId,
+          currentPage,
+        });
+        navigate(`?${queryString}`);
+      }
+      isMounted.current = true;
+    }, [categoryId, sortType, currentPage]);
+
+
+//Если был первые рендер то проверяем URL и сохраняем в redux
   useEffect(() => {
-    setIsLoading(true);
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
 
-    const order = sortType.includes('-') ? 'asc' : 'desc';
-    const sortBy = sortType.replace('-', '');
-    const category = categoryId > 0 ? `category=${categoryId}` : '';
-    const search = searchValue ? `&search=${searchValue}` : '';
+      const sort = menuList.find(obj => obj.sortProperty === params.sortProperty);
+      
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        })
+      )
+      isSearch.current = true;
+    }
+  }, []);
 
-    axios.get(
-      `https://63395945937ea77bfdc99bb3.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
-      .then((res) => {
-        setItems(res.data);
-        setIsLoading(false);
-      })
-
+  //Если был первый рендер то делаем запрос на сервер
+  useEffect(() => {
     window.scrollTo(0, 0);
+
+    if (!isSearch.current) {
+      fetchPizzas();
+    }
+
+    isSearch.current = false;
+
   }, [categoryId, sortType, searchValue, currentPage]);
+
+
 
   const pizzas = items.map((obj) => <PizzaBlock {...obj} key={obj.id} />);
   const sceletons = [...new Array(6)].map((_, index) => <Sceleton key={index} />);
